f7969cdacc509df363f0070294f60e73975d035a
OMEN-16\Admin <mitphan1110@gmail.com>
2026-05-05 19:35:29 +0700

    chỉnh sửa path mặc định của các model trong predictor.py

--- realtime/predictor.py
+++ realtime/predictor.py
@@ -264,8 +264,8 @@ if __name__ == "__main__":
 
     parser.add_argument("--csv", default="collected_data.csv")
     parser.add_argument("--scaler", default="model/scaler.joblib")
-    parser.add_argument("--ae", default="model/autoencoder.pth")
-    parser.add_argument("--rf", default="model/rf_model.pkl")
+    parser.add_argument("--ae", default="model/ae_model.pth")
+    parser.add_argument("--rf", default="model/rf_model.joblib")
     parser.add_argument("--block-url", default="http://127.0.0.1:8080/block")
     parser.add_argument("--idle-timeout", type=int, default=300)
     parser.add_argument("--interval", type=float, default=1.0)
